1f42ef30f763aa42476952c4ceb54f3718e6471e
LayNath242 <laynath242@gmail.com>
2020-07-28 14:17:25 +0700

    stack and heap

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,9 +1,11 @@
 pub mod core_data_type;
 pub mod operators;
 pub mod scope_and_shadowing;
+pub mod stack_heap;
 
 fn main() {
     // core_data_type::core_data_type();
     // operators::operator();
-    scope_and_shadowing::scope_gobal();
+    // scope_and_shadowing::scope_gobal();
+    stack_heap::stack_and_heap();
 }
